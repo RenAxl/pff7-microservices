a4024ca0562bc12993337aaccdd6460c4f9ad064
Renan Duarte <renandg.axl@gmail.com>
2023-03-21 17:09:36 -0300

    Backend - Initial settings and creation of pff7-user project entity layer

---
+++ pff7-user/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Axl', 'axl@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Rose', 'rose@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_ANALYST');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
