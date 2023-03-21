aff211ec45cb3d7ea71c9bc4cdbef028488d539e
Renan Duarte <renandg.axl@gmail.com>
2023-03-21 12:44:55 -0300

    Creating the pff7-user project

---
+++ pff7-user/src/main/java/com/example/pff7user/Pff7UserApplication.java
@@ -0,0 +1,13 @@
+package com.example.pff7user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pff7UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pff7UserApplication.class, args);
+	}
+
+}
---
+++ pff7-user/src/test/java/com/example/pff7user/Pff7UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pff7user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pff7UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
