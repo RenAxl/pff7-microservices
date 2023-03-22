91849d1b70c69165f7481ed145344279c2348fd0
Renan Duarte <renandg.axl@gmail.com>
2023-03-22 18:58:10 -0300

    Backend: Creating the pff7-config-server project

---
+++ pff7-config-server/src/main/java/com/thayren/pff7configserver/Pff7ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pff7ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pff7ConfigServerApplication.class, args);
+	}
+
+}
---
+++ pff7-config-server/src/test/java/com/thayren/pff7configserver/Pff7ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pff7ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
