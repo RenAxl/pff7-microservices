312adc769f44059e1c81834df8e8d2f9b3b4b46f
Renan Duarte <renandg.axl@gmail.com>
2023-03-23 18:02:31 -0300

    Backend: Creating the pff7-character project

---
+++ pff7-character/src/main/java/com/thayren/pff7character/Pff7CharacterApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7character;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pff7CharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pff7CharacterApplication.class, args);
+	}
+
+}
---
+++ pff7-character/src/test/java/com/thayren/pff7character/Pff7CharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7character;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pff7CharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
