8d995800f02ef52597a0cca0409d0553d1de7668
Renan Duarte <renandg.axl@gmail.com>
2023-03-23 11:56:36 -0300

    Backend: Creating the pff7-eureka-server project

---
+++ pff7-eureka-server/src/main/java/com/thayren/pff7eurekaserver/Pff7EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pff7EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pff7EurekaServerApplication.class, args);
+	}
+
+}
---
+++ pff7-eureka-server/src/test/java/com/thayren/pff7eurekaserver/Pff7EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pff7EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
