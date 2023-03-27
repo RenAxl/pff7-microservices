76ce0d1ee0db3dacf9f7f0c582f119c637ffc6f7
Renan Duarte <renandg.axl@gmail.com>
2023-03-27 19:41:36 -0300

    Backend: Creating the pff7-authorization-server

---
+++ pff7-authorization-server/src/main/java/com/thayren/pff7authorizationserver/Pff7AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.pff7authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pff7AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pff7AuthorizationServerApplication.class, args);
+	}
+
+}
