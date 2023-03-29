319d351082d7e4869dd465b4ff13411e61b9f6f9
Renan Duarte <renandg.axl@gmail.com>
2023-03-29 12:23:22 -0300

    Backend: User Authentication and JWT token generation.

--- pff7-authorization-server/src/main/java/com/thayren/pff7authorizationserver/resources/UserResource.java
+++ pff7-authorization-server/src/main/java/com/thayren/pff7authorizationserver/resources/UserResource.java
@@ -21,7 +21,7 @@ public class UserResource {
 	@GetMapping(value = "/search")
 	public ResponseEntity<User> findByEmail(@RequestParam String email) {
 		try {
-			User user = service.findByEmail(email);
+			User user = service.loadUserByUsername(email);
 			return ResponseEntity.ok(user);
 		} catch (IllegalArgumentException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
