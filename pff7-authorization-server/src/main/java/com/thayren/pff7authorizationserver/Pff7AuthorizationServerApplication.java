b2f1ec075d429b54a214ab3e16b13040de6ade30
Renan Duarte <renandg.axl@gmail.com>
2023-03-29 10:44:16 -0300

    Backend: Configuring the pff7-authorization-server microservice as a Eureka Server client.

--- pff7-authorization-server/src/main/java/com/thayren/pff7authorizationserver/Pff7AuthorizationServerApplication.java
+++ pff7-authorization-server/src/main/java/com/thayren/pff7authorizationserver/Pff7AuthorizationServerApplication.java
@@ -2,7 +2,9 @@ package com.thayren.pff7authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Pff7AuthorizationServerApplication {
 
