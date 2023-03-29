3d415ee36b6f9edc9d7e7bfdefc1eebde8302edb
Renan Duarte <renandg.axl@gmail.com>
2023-03-29 16:05:48 -0300

    Backend: Configuring resource server in Zuul api gateway

---
+++ pff7-api-gateway-zuul/src/main/java/com/thayren/pff7apigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,76 @@
+package com.thayren.pff7apigatewayzuul.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private JwtTokenStore tokenStore;
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) { 
+			http.headers().frameOptions().disable();
+		}
+
+		http.authorizeRequests()
+		.antMatchers("/pff7-authorization-server/oauth/token", "/h2-console/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/pff7-character/categories/**", "/pff7-character/characters/**").permitAll()
+		.antMatchers(HttpMethod.POST, "/pff7-character/categories/**", "/pff7-character/characters/**").hasAnyRole("ANALYST", "ADMIN")
+		.antMatchers(HttpMethod.PUT, "/pff7-character/categories/**", "/pff7-character/characters/**").hasAnyRole("ANALYST", "ADMIN")
+		.antMatchers(HttpMethod.DELETE, "/pff7-character/categories/**", "/pff7-character/characters/**").hasAnyRole("ANALYST", "ADMIN")
+		.antMatchers("/pff7-user/users/**").hasRole("ADMIN")
+		.anyRequest().authenticated();
+		
+		http.cors().configurationSource(corsConfigurationSource());
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*")); 
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean 
+			= new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+}
